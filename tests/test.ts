3790a6de1ab3a8446442f9b0308ae7bbe14d8d43
Coty Hamilton <cotyhamilton@gmail.com>
2023-11-06 23:42:46 -0500

    configure tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,28 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected h1", async ({ page }) => {
+// authenticate
+test.beforeEach(async ({ page }) => {
 	await page.goto("/");
-	await expect(page.getByRole("heading", { name: "budgety 💰" })).toBeVisible();
+	if (!process.env.DIRECTUS_EMAIL || !process.env.DIRECTUS_PASSWORD) {
+		throw Error("Set admin email and password environment variables");
+	}
+	await page.getByLabel("email").fill(process.env.DIRECTUS_EMAIL);
+	await page.getByLabel("password").fill(process.env.DIRECTUS_PASSWORD);
+	await page.getByRole("button", { name: "log in" }).click();
+	await page.waitForURL("/accounts");
+});
+
+test("accounts", async ({ page }) => {
+	await page.goto("/accounts");
+	await expect(page.getByRole("heading", { name: "accounts" })).toBeVisible();
+});
+
+test("boxes", async ({ page }) => {
+	await page.goto("/boxes");
+	await expect(page.getByRole("heading", { name: "boxes" })).toBeVisible();
+});
+
+test("transactions", async ({ page }) => {
+	await page.goto("/transactions");
+	await expect(page.getByRole("heading", { name: "transactions" })).toBeVisible();
 });
