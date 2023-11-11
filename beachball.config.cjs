51af5ae4ca92096e1ae689c5301fb9954d7dfd87
Coty Hamilton <cotyhamilton@gmail.com>
2023-11-11 00:07:26 -0500

    Beachball setup

--- beachball.config.cjs
+++ beachball.config.cjs
@@ -2,4 +2,11 @@
 /** @type {import("beachball").BeachballConfig}*/
 module.exports = {
 	branch: "origin/main",
+	disallowedChangeTypes: ["major", "minor", "patch"],
+	publish: false,
+	changelog: {
+		customRenderers: {
+			renderEntry: (entry, _renderInfo) => `- ${entry.comment}`
+		}
+	},
 }
