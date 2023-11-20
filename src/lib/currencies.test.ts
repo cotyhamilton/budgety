4552e2cd2a90adc8864c5f2cee612c1ebea0782c
Coty Hamilton <cotyhamilton@gmail.com>
2023-11-19 20:54:31 -0500

    Formatting

--- src/lib/currencies.test.ts
+++ src/lib/currencies.test.ts
@@ -2,13 +2,13 @@ import { describe, expect, it } from "vitest";
 import { convertFromSubunits, convertToSubunits, formatter } from "./currencies";
 
 describe("currencie functions", () => {
-    it("should convert currency to subunits", async () => {
-        expect(convertToSubunits(1000, 2)).toEqual(100000);
-    });
-    it("should convert currency from subunits", async () => {
-        expect(convertFromSubunits(1200000, 3)).toEqual(1200);
-    });
-    it("should format currency", async () => {
-        expect(formatter("USD").format(1000)).toEqual("$1,000.00");
-    })
+	it("should convert currency to subunits", async () => {
+		expect(convertToSubunits(1000, 2)).toEqual(100000);
+	});
+	it("should convert currency from subunits", async () => {
+		expect(convertFromSubunits(1200000, 3)).toEqual(1200);
+	});
+	it("should format currency", async () => {
+		expect(formatter("USD").format(1000)).toEqual("$1,000.00");
+	});
 });
--- tests/navigation.fixture.ts
+++ tests/navigation.fixture.ts
@@ -1,17 +1,17 @@
-import type { Page } from '@playwright/test';
+import type { Page } from "@playwright/test";
 
 export class Navigation {
-    constructor(public readonly page: Page) { }
+	constructor(public readonly page: Page) {}
 
-    async createAccount() {
-        await this.page.goto("/");
-        await this.page.getByRole("button", { name: "log in" }).click();
-        await this.page.getByPlaceholder("capital one").click();
-        await this.page.getByPlaceholder("capital one").fill("capital one");
-        await this.page.getByRole("combobox").click();
-        await this.page.getByRole("option", { name: "USD" }).click();
-        await this.page.getByLabel("current balance").click();
-        await this.page.getByLabel("current balance").fill("1000");
-        await this.page.getByRole("button", { name: "save" }).click();
-    }
+	async createAccount() {
+		await this.page.goto("/");
+		await this.page.getByRole("button", { name: "log in" }).click();
+		await this.page.getByPlaceholder("capital one").click();
+		await this.page.getByPlaceholder("capital one").fill("capital one");
+		await this.page.getByRole("combobox").click();
+		await this.page.getByRole("option", { name: "USD" }).click();
+		await this.page.getByLabel("current balance").click();
+		await this.page.getByLabel("current balance").fill("1000");
+		await this.page.getByRole("button", { name: "save" }).click();
+	}
 }
