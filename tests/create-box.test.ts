e05f721be596b648a8ef59b685a26af48cf4fe50
Coty Hamilton <cotyhamilton@gmail.com>
2023-12-09 15:00:10 -0700

    Update tests

--- tests/create-box.test.ts
+++ tests/create-box.test.ts
@@ -7,8 +7,8 @@ test("can create box", async ({ page, navigation }) => {
 	await page.getByText("add box").click();
 	await page.locator("#name").fill("coffee");
 	await page.locator("#amount").click();
-	await page.locator("#amount").fill("900000");
+	await page.locator("#amount").fill("100");
 	await page.getByRole("button", { name: "Save changes" }).click();
 
-	await expect(page.getByText("coffee 0 / 900000")).toBeVisible();
+	await expect(page.getByText("coffee $0.00 / $100.00")).toBeVisible();
 });
